442901f9940ab3e0b63cd2144468f417896199eb
walmer-almeida <42725069+walmer-almeida@users.noreply.github.com>
2019-08-21 17:39:09 -0300

    compress iniciada

--- Principal.c
+++ Principal.c
@@ -1,31 +1,41 @@
 #include "Structures.h"
-void insert_heap(hash_table *ht){
-    heap *h= create_heap();
-    int i;
-    for(i=0; i<HASHSIZE; i++){
-        if(ht->table[i]->frequency != 0){
-            enqueue(h, ht->table[i]);
-        }else{
-            free(ht->table[i]);
-        }
-    }
-}
-int main(){
-    FILE *file;
-    file= fopen("teste.txt", "rb");
+void Compress(FILE *file, char string_file[]){
+    printf("Enter file name\n");
+
     if(file == NULL){
-        printf("Arquivo não encontrado\n");
+        printf("File not found\n");
         return 0;
-    }
+    }printf("a");
     unsigned char c;
     hash_table *ht= create_hash_table();
     while(!feof(file)){
         c= fgetc(file);
         put(ht, c);
     }
-    insert_heap(ht);
-    //print_hash_table(ht);
-
+    heap *h;
+    h= insert_heap_from_hash(ht);
+    huffTree *bt;
+    bt= create_Tree_from_heap(h);
+    free(h);
+    printTree(bt);
     fclose(file);
+}
+int main(){
+    char string_file[100];
+    int type= 0;
+    FILE *file;
+    printf("Do you want to compress(1) or decompress(2)?\n");
+    scanf("%d", type);
+    getchar();
+    if(type == 1){
+        printf("Enter file name\n");
+        scanf("%[^\n]s", string_file);
+        file= fopen(string_file, "rb");
+        Compress(file, string_file);
+    }else if(type == 2){
+        printf("a");
+    }else{
+        printf("b");
+    }
     return 0;
 }
