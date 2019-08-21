4c73fd50f311e86774dad65a90e09dcb59ebfcc4
walmer-almeida <42725069+walmer-almeida@users.noreply.github.com>
2019-08-20 23:58:05 -0300

    adição da função que insere os elementos na heap

--- Principal.c
+++ Principal.c
@@ -1,6 +1,31 @@
 #include "Structures.h"
-
+void insert_heap(hash_table *ht){
+    heap *h= create_heap();
+    int i;
+    for(i=0; i<HASHSIZE; i++){
+        if(ht->table[i]->frequency != 0){
+            enqueue(h, ht->table[i]);
+        }else{
+            free(ht->table[i]);
+        }
+    }
+}
 int main(){
-    
+    FILE *file;
+    file= fopen("teste.txt", "rb");
+    if(file == NULL){
+        printf("Arquivo não encontrado\n");
+        return 0;
+    }
+    unsigned char c;
+    hash_table *ht= create_hash_table();
+    while(!feof(file)){
+        c= fgetc(file);
+        put(ht, c);
+    }
+    insert_heap(ht);
+    //print_hash_table(ht);
+
+    fclose(file);
     return 0;
 }
